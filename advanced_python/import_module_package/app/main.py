366f1a594428255f95638ca7107d9578c33517ac
Nikita Davydov <davydovnikitav@gmail.com>
2025-07-26 10:18:55 +0300

    добавил задание по теме декораторы

--- advanced_python/import_module_package/app/main.py
+++ advanced_python/import_module_package/app/main.py
@@ -1,5 +1,7 @@
 from advanced_python.import_module_package.app.application.salary import person_salary
+from advanced_python.logger.logger_2 import logger
 
+@logger('main.log')
 def main():
     return print(person_salary())
 
